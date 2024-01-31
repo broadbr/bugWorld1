721d041699cab771b50db97c9049dca8d98dde98
Colin Swain <colinmswain@gmail.com>
2024-01-30 20:57:18 -0500

    added getHealth to bug file

---
+++ src/ant.java
@@ -0,0 +1,12 @@
+public class ant extends bug{
+    /* Values are subject to change
+    private String name = "ant";
+    private int damage = 1;
+    private int cost = 1; 
+    private int health = 1;
+    */
+    
+    ant(){
+    super("ant", 1, 1, 1);
+    }
+}
