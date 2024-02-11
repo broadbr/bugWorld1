96a2d0b26bf0e18ee9ceab78dd967933f0d8f382
Marcus B. <bollacm@sunypoly.edu>
2024-02-10 20:56:05 -0500

    commit

--- src/window.java
+++ src/window.java
@@ -2,15 +2,31 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.Timer;
 import java.util.*;
+import java.awt.event.*;
 
-public class window {
+public class window{
+
+    bugList buglist = new bugList();
 
     public window(int width, int height, String title, Main game) {
         //set frame name title
+
         JFrame frame = new JFrame(title);
         JFrame buttonFrame = new JFrame();
 
-        JButton myButton = new JButton("test");
+        JButton myButton = new JButton("buy ant");
+
+        /*
+        myButton.addActionListener(new ActionListener() {
+            @Override public void actionPerformed(ActionEvent e) {
+                if (e.getSource() == myButton) {
+          //How can I call an ant to spawn here?
+
+                }
+            }
+        }
+*/
+
         buttonFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         buttonFrame.getContentPane().add(myButton);
         buttonFrame.setSize(200,200);
