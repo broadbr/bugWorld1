e5e0f4814ed7b676b513db941bfcfe79c5cd41ab
Marcus B. <bollacm@sunypoly.edu>
2024-02-10 19:37:26 -0500

    commit

--- src/window.java
+++ src/window.java
@@ -8,8 +8,14 @@ public class window {
     public window(int width, int height, String title, Main game) {
         //set frame name title
         JFrame frame = new JFrame(title);
+        JFrame buttonFrame = new JFrame();
+
         JButton myButton = new JButton("test");
-        myButton.setPreferredSize(new Dimension(10,10));
+        buttonFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        buttonFrame.getContentPane().add(myButton);
+        buttonFrame.setSize(200,200);
+        buttonFrame.setVisible(true);
+
         //set size
         frame.setPreferredSize(new Dimension(width, height));
         frame.setMaximumSize(new Dimension(width, height));
