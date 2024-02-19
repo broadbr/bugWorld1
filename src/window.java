bc99961c2eed9060541fc51a78c84b536e9d16a7
Marcus B. <bollacm@sunypoly.edu>
2024-02-19 13:40:26 -0500

    Added upgrade window

--- src/window.java
+++ src/window.java
@@ -8,22 +8,38 @@ public class window {
 
         JButton antButton = new JButton("Ant purchase");
         JButton beetleButton = new JButton("Beetle purchase");
+        JButton antUpgrade = new JButton("Upgrade Ant");
+        JButton beetleUpgrade = new JButton("Upgrade Beetle");
+        JButton levelUp = new JButton("Level Up");
 
         JLabel shopName = new JLabel("Insect Shop: ");
+        JLabel upgradeName = new JLabel("Upgrades: ");
 
         Dimension buttonA = new Dimension(150, 100);
         antButton.setPreferredSize(buttonA);
         beetleButton.setPreferredSize(buttonA);
+        antUpgrade.setPreferredSize(buttonA);
 
         JPanel leftPanel = new JPanel();
         leftPanel.setBackground(Color.GRAY);
         leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
-        //leftPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // Top, Left, Bottom, Right padding
         leftPanel.add(shopName);
         leftPanel.add(Box.createRigidArea(new Dimension(0, 30)));
         leftPanel.add(antButton);
         leftPanel.add(Box.createRigidArea(new Dimension(0, 10)));
         leftPanel.add(beetleButton);
+
+        JPanel rightPanel = new JPanel();
+        rightPanel.setBackground(Color.GRAY);
+        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+        rightPanel.add(upgradeName);
+        rightPanel.add(Box.createRigidArea(new Dimension(0, 30)));
+        rightPanel.add(antUpgrade);
+        rightPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+        rightPanel.add(beetleUpgrade);
+        rightPanel.add(Box.createRigidArea(new Dimension(0, 390)));
+        rightPanel.add(levelUp);
+
 
         frame.setLayout(new BorderLayout());
         frame.setPreferredSize(new Dimension(width,height));
@@ -32,6 +48,7 @@ public class window {
         
         frame.add(game);
         frame.add(leftPanel, BorderLayout.WEST);
+        frame.add(rightPanel, BorderLayout.EAST);
         frame.setResizable(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
