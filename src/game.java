7206185d5a00a0983ae8e5d786180c4c7f9cba09
Marcus B <bollacm@sunypoly.edu>
2024-03-11 12:40:58 -0400

    commit

--- src/game.java
+++ src/game.java
@@ -17,6 +17,7 @@ public class game extends Canvas implements Runnable {
     private stage1 stage1;
     private stage2 stage2;
     public int activeStage =1;
+    //public int activeStage =0;
 
     public game() {
         new window(1360,640,"Bug World 1",this);//shop+upgrades = 100x2+width = 24x20 = 480+200=680 & height = 16x20=320
@@ -83,6 +84,24 @@ public class game extends Canvas implements Runnable {
         if(activeStage==2){
             stage2.makeFood();// runs stage 2
         }
+        /* Cycle 2
+
+
+        if(activeStage==1) {
+            stage1.makeFood();// runs stage 1
+        }
+        if(activeStage==2) {
+            stage1.makeFood2();// run S 1 L 2
+        }
+        if(activeStage==3){
+            stage2.makeFood();// runs stage 2
+        }
+        if(activeStage==4){
+            stage2.makeFood2();// run S2 L2
+        }
+
+
+        */
 
     }
 
@@ -107,8 +126,35 @@ public class game extends Canvas implements Runnable {
         }
         if(activeStage == 2) {
             Image image = Toolkit.getDefaultToolkit().getImage("src/assets/csStage2.png");
-            g.drawImage(image,-70,0,null); // loads stage 
+            g.drawImage(image,-70,0,null); // loads stage
         }
+
+        /*
+        if(activeStage == 0) {
+            Image image = Toolkit.getDefaultToolkit().getImage("menu.png");
+            g.drawImage(image,150,100,null); // loads menu png
+        }
+        if(activeStage == 1) {
+            Image image = Toolkit.getDefaultToolkit().getImage("src/assets/Stage1AssetTemp.png");
+            g.drawImage(image,150,100,null); // loads stage 1 L1
+
+        if(activeStage == 2) {
+            Image image = Toolkit.getDefaultToolkit().getImage("src/assets/Stage1Asset2.png");
+            g.drawImage(image,150,100,null);  // loads stage 1 L2
+        }
+
+        if(activeStage == 3) {
+            Image image = Toolkit.getDefaultToolkit().getImage("src/assets/Stage2Asset1.png");
+            g.drawImage(image,150,100,null); // loads stage 2 L1
+
+        if(activeStage == 4) {
+            Image image = Toolkit.getDefaultToolkit().getImage("src/assets/Stage2Asset2.png");
+            g.drawImage(image,-70,0,null); // loads stage 2 L2
+
+        */
+
+
+
 
         // Draw portal
         Image image2 = Toolkit.getDefaultToolkit().getImage("src/assets/portal.png");
